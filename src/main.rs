3ac9c1ca1745440319c9b9291f86a3ce566b13e8
Sheen An <sheen.an.goh@gmail.com>
2024-02-15 00:00:23 +0800

    [stage-5] naive implementation

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,49 @@ use std::{
     thread,
 };
 
+struct RESPDataType;
+
+impl RESPDataType {
+    const ARRAY: u8 = b'*';
+    const BULK: u8 = b'$';
+}
+
+fn evaluate_resp(mut cmd: &[u8]) -> String {
+    //  *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n
+
+    let mut len: u8 = 0;
+    if cmd[0] == RESPDataType::ARRAY {
+        len = cmd[1] - b'0';
+        //  $4\r\nECHO\r\n$3\r\nhey\r\n
+        cmd = &cmd[4..];
+    }
+
+    match cmd[0] {
+        RESPDataType::BULK => {
+            let args = get_args(cmd, len);
+            match &args[0] {
+                x if x == "ping" => "+PONG\r\n".to_string(),
+                x if x == "echo" => format!("+{}\r\n", args[1]),
+                _ => "-not_supported command".to_string(),
+            }
+        }
+        _ => "-not_supported data type\r\n".to_string(),
+    }
+}
+
+fn get_args(mut cmd: &[u8], mut len: u8) -> Vec<String> {
+    let mut args: Vec<String> = Vec::new();
+    while len > 0 {
+        // $4\r\nECHO\r\n$3\r\nhey\r\n
+        let arg_len: usize = (cmd[1] - b'0') as usize;
+        let arg = String::from_utf8_lossy(&cmd[4..arg_len + 4]);
+        args.push(arg.into());
+        cmd = &cmd[arg_len + 4 + 2..];
+        len -= 1;
+    }
+    args
+}
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -22,10 +65,10 @@ fn main() {
 }
 
 fn handle_stream(stream: &mut TcpStream) {
-    let response: &str = "+PONG\r\n";
-    let mut buffer: [u8; 1024] = [0; 1024];
+    let mut buffer = [0u8; 120];
 
     while let Ok(_) = stream.read(&mut buffer) {
-        _ = stream.write(&response.as_bytes())
+        let val: String = evaluate_resp(&mut buffer);
+        _ = stream.write(&val.as_bytes())
     }
 }
