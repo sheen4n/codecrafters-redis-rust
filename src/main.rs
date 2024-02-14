583567f1570e6524db975b3e68d94b2ae068e52e
Sheen An <sheen.an.goh@gmail.com>
2024-02-14 22:02:05 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-// Uncomment this block to pass the first stage
-// use std::net::TcpListener;
-
 use std::{
     io::{Read, Write},
     net::{TcpListener, TcpStream},
@@ -10,23 +7,29 @@ use std::{
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-    let listener = TcpListener::bind("127.0.0.1:6379").expect("could not bind");
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:6379").expect("could not bind");
     for stream in listener.incoming() {
-        let mut stream = stream.expect("failed to accept");
-        thread::spawn(move || {
-            handle_connection(&mut stream);
-        });
+        match stream {
+            Ok(mut stream) => {
+                thread::spawn(move || {
+                    handle_connection(&mut stream);
+                });
+            }
+            Err(e) => {
+                println!("error: {e}");
+            }
+        }
     }
 }
 
 fn handle_connection(stream: &mut TcpStream) {
-    let mut buffer = [0; 1024];
+    let mut buffer: [u8; 1024] = [0; 1024];
     loop {
-        let bytes_read = stream.read(&mut buffer).expect("read failure");
+        let bytes_read: usize = stream.read(&mut buffer).expect("read failure");
         if bytes_read == 0 {
             break;
         }
-        let response = "+PONG\r\n";
+        let response: &str = "+PONG\r\n";
         stream
             .write_all(response.as_bytes())
             .expect("write failure");
